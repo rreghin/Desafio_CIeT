f7a58d83a2f1b30e51bd98e789435646e532adde
Roger Reghin <rreghin@gmail.com>
2021-04-18 00:52:00 -0300

    Segundo teste

--- test/ScheduleJob.test.ts
+++ test/ScheduleJob.test.ts
@@ -7,3 +7,10 @@ test("Schema should match", async function() {
     const job = await validateSchema.execute(1);
     expect(job.id).toBe(1);
 });
+
+test("Should have an MaxStartDate", async function() {
+    const jobRepositoryMemory = new JobRepositoryMemory();
+    const validateSchema = new ValidateSchema(jobRepositoryMemory);
+    const job = await validateSchema.execute(1);
+    expect(job.maxStartDate.toISOString()).toBe("2021-04-18T01:30:00.000Z");
+});
